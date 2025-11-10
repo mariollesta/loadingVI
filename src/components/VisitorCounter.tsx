5c15bb46140b17cc34d7bc02f46babee73d6ec9a
mariollesta <llesta.mario@gmail.com>
2025-11-10 18:36:22 +0100

    feat: configure uptash kv db in Vercel for visitor counter component

--- src/components/VisitorCounter.tsx
+++ src/components/VisitorCounter.tsx
@@ -1,17 +1,20 @@
 import { useState, useEffect } from 'react'
 
-
 export const VisitorCounter: React.FC = () => {
     const [visits, setVisits] = useState<string>('...')
 
     useEffect(() => {
         const fetchVisits = async () => {
             try {
-                const response = await fetch(`https://...`)
+                const response = await fetch('/api/visits')
+                
+                if (!response.ok) {
+                    throw new Error('Failed to fetch visits')
+                }
                 
                 // type the expected response from the API
                 const data: { value: number } = await response.json()
-                setVisits(data.value.toString())
+                setVisits(data.value.toLocaleString()) 
 
             } catch (error) {
                 console.error("Error fetching visitor count:", error)
@@ -24,10 +27,10 @@ export const VisitorCounter: React.FC = () => {
 
     return (
         <div className="text-center">
-            <p className="text-white opacity-70 text-base">
+            <p className="text-white opacity-70 text-sm sm:text-base">
                 VISITAS A LA WEB: 
-                <span className="text-lg text-[var(--color-gold)] ml-2">
-                {visits}
+                <span className="text-base sm:text-lg text-[var(--color-gold)] ml-2 font-bold">
+                    {visits}
                 </span>
             </p>
         </div>
